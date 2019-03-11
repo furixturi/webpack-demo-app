16c9cc68557d03785fc6b7063f46486fcc3dd6a4
Xiaoli Shen <alabebop@gmail.com>
2019-03-11 21:33:56 +0900

    Update webpack.common.js
    Add comments on what "html-loader" and "file-loader" are for

--- webpack.common.js
+++ webpack.common.js
@@ -10,12 +10,12 @@ module.exports = {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ["html-loader"] // html-loader to automatically require the files we reference in img tags
       },
       {
         test: /\.(svg|png|jpg|gif)$/,
         use: {
-          loader: "file-loader",
+          loader: "file-loader",  // file-loader to handle the svg,png,jpg,gif files from our images
           options: {
             name: "[name].[hash].[ext]",
             outputPath: "imgs"
